60b9f156f3605a4923bf50299a0f3dec70dcaf18
Jesper Simonsson <jsimo@kth.se>
2019-04-18 11:30:03 +0200

    Adds system metrics csv export
    - Network traffic bytes in/out
    - CPU usage
    - Memory usage
    - IO usage

--- chaosorca/orc/metrics/commands.py
+++ chaosorca/orc/metrics/commands.py
@@ -8,11 +8,44 @@ def metric():
     pass
 
 @metric.command()
-def syscall():
-    '''Returns csv date from prometheus for syscalls'''
-    prometheus_metrics.syscallQuery()
+@click.option('--name', prompt='Container name?')
+def syscall(name):
+    '''Returns csv data from prometheus for syscalls'''
+    prometheus_metrics.syscallQuery(name)
 
 @metric.command()
-def network():
-    '''Returns csv date from prometheus for network'''
-    prometheus_metrics.networkQuery()
+@click.option('--name', prompt='Container name?')
+def http(name):
+    '''Returns csv data from prometheus for HTTP traffic'''
+    prometheus_metrics.networkQuery(name)
+
+@metric.command()
+@click.option('--name', prompt='Container name?')
+def cpu(name):
+    '''Returns csv data from prometheus for cpu usage'''
+    prometheus_metrics.cpuQuery(name)
+
+@metric.command()
+@click.option('--name', prompt='Container name?')
+def mem(name):
+    '''Returns csv data from prometheus for cpu usage'''
+    prometheus_metrics.memQuery(name)
+
+@metric.command()
+@click.option('--name', prompt='Container name?')
+def netr(name):
+    '''Returns csv data from prometheus for network receive'''
+    prometheus_metrics.netreceiveQuery(name)
+
+@metric.command()
+@click.option('--name', prompt='Container name?')
+def nets(name):
+    '''Returns csv data from prometheus for network send'''
+    prometheus_metrics.nettransmitQuery(name)
+
+@metric.command()
+@click.option('--name', prompt='Container name?')
+def io(name):
+    '''Returns csv data from prometheus for io activity'''
+    prometheus_metrics.ioQuery(name)
+
--- chaosorca/orc/metrics/prometheus_metrics.py
+++ chaosorca/orc/metrics/prometheus_metrics.py
@@ -6,22 +6,24 @@ import time
 PROMETHEUS_URL = 'http://localhost:9090/api/v1/%s'
 PROMETHEUS_QUERY = PROMETHEUS_URL % 'query'
 PROMETHEUS_QUERY_RANGE = PROMETHEUS_URL % 'query_range'
-RATEQUERY_SYSCALL = 'sum by (syscall) (rate(syscall_counter_total[%ss]))'
-RATEQUERY_NETWORK = 'sum by (response_code) (rate(http_request_total[%ss]))'
+RATEQUERY_SYSCALL = 'sum by (syscall) (rate(syscall_counter_total{job="%s"}[%ss]))'
+RATEQUERY_NETWORK = 'sum by (response_code) (rate(http_request_total{job="%s"}[%ss]))'
 
-def testQuery():
-    '''Send test query to prometheus'''
-    r = requests.get(url=PROMETHEUS_URL, params={'query': 'up'})
-    return r.json()
+RATEQUERY_SYSTEM_CPU = 'sum by (name) (rate(container_cpu_user_seconds_total{name="%s"}[%ss]))'
+QUERY_SYSTEM_MEM = 'container_memory_usage_bytes{name="%s"}'
+RATEQUERY_NETWORK_RECEIVE = 'sum by (name) (rate(container_network_receive_bytes_total{name="%s"}[%ss]))'
+RATEQUERY_NETWORK_TRANSMIT = 'sum by (name) (rate(container_network_transmit_bytes_total{name="%s"}[%ss]))'
+RATEQUERY_SYSTEM_IO = 'sum by (name) (rate(container_fs_io_time_seconds_total{name="%s"}[%ss]))'
 
 def toCsv(json):
     '''Convert the json result to csv, prints the resulting csv to stdout'''
-    #writer = csv.writer(sys.stdout)
-
     results = json['data']['result']
     labels = set()
     for res in results:
-        labels.update(res['metric'].values())
+        if '__name__' in res['metric']:
+            labels.update([res['metric']['__name__']])
+        else:
+            labels.update(res['metric'].values())
     labels = sorted(labels)
     labels = ['timestamp'] + labels
 
@@ -41,37 +43,87 @@ def toCsv(json):
         writer.writerow(dictobj)
 
 
-def syscallQuery(timespan=15*60, rate=60, range=3):
+# Syscalls & HTTP networking queries.
+
+def syscallQuery(name, timespan=15*60, rate=60, step=3):
     '''Queries prometheus for the rate of syscall queries.'''
     current_time = int(round(time.time()))
     end_time = current_time
     start_time = current_time - timespan
-    step = 3
     if (end_time - start_time) / step > 10000:
         print('Please do a shorter range')
         # As maximum sample size in prometheus is 11000.
         return
 
-    query = RATEQUERY_SYSCALL % rate
+    query = RATEQUERY_SYSCALL % (name, rate)
     r = requests.get(url=PROMETHEUS_QUERY_RANGE,
         params={'query': query, 'start':start_time, 'end':end_time, 'step':step})
     res_json = r.json()
     toCsv(res_json)
 
-def networkQuery(timespan=15*60, rate=60, range=3):
+def networkQuery(name, timespan=15*60, rate=60, step=3):
     '''Queries prometheus for the network.'''
     current_time = int(round(time.time()))
     end_time = current_time
     start_time = current_time - timespan
-    step = 3
     if (end_time - start_time) / step > 10000:
         print('Please do a shorter range')
         # As maximum sample size in prometheus is 11000.
         return
 
-    query = RATEQUERY_NETWORK % rate
+    query = RATEQUERY_NETWORK % (name, rate)
     r = requests.get(url=PROMETHEUS_QUERY_RANGE,
         params={'query': query, 'start':start_time, 'end':end_time, 'step':step})
     res_json = r.json()
     toCsv(res_json)
 
+# System queries;
+# - Network activity
+# - Cpu usage
+# - Memory usage
+# - IO activity
+
+def systemQuery(query, timespan, step):
+    current_time = int(round(time.time()))
+    end_time = current_time
+    start_time = current_time - timespan
+
+    if (end_time - start_time) / step > 10000:
+        print('Please do a shorter range')
+        # As maximum sample size in prometheus is 11000.
+        return
+
+    r = requests.get(url=PROMETHEUS_QUERY_RANGE,
+        params={'query': query, 'start':start_time, 'end':end_time, 'step':step})
+    res_json = r.json()
+    return res_json
+
+def cpuQuery(name, timespan=15*60, rate=60, step=3):
+    '''Queries Prometheus for the cpu usage'''
+    query = RATEQUERY_SYSTEM_CPU % (name, rate)
+    res_json = systemQuery(query, timespan, step)
+    toCsv(res_json)
+
+def memQuery(name, timespan=15*60, step=3):
+    '''Queries Prometheus for the memory usage'''
+    query = QUERY_SYSTEM_MEM % (name)
+    res_json = systemQuery(query, timespan, step)
+    toCsv(res_json)
+
+def netreceiveQuery(name, timespan=15*60, rate=60, step=3):
+    '''Queries Prometheus for the incoming network activity'''
+    query = RATEQUERY_NETWORK_RECEIVE % (name, rate)
+    res_json = systemQuery(query, timespan, step)
+    toCsv(res_json)
+
+def nettransmitQuery(name, timespan=15*60, rate=60, step=3):
+    '''Queries Prometheus for the outgoing network activity'''
+    query = RATEQUERY_NETWORK_TRANSMIT % (name, rate)
+    res_json = systemQuery(query, timespan, step)
+    toCsv(res_json)
+
+def ioQuery(name, timespan=15*60, rate=60, step=3):
+    '''Queries Prometheus for the container io activity'''
+    query = RATEQUERY_SYSTEM_IO % (name, rate)
+    res_json = systemQuery(query, timespan, step)
+    toCsv(res_json)
