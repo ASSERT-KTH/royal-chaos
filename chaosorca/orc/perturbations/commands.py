c9812bc7677d2e4cedfd0ad6c40816e7842ee691
Jesper Simonsson <jsimo@kth.se>
2019-04-23 10:58:21 +0200

    Adds premade list of commands for testing on applications

--- chaosorca/orc/perturbations/commands.py
+++ chaosorca/orc/perturbations/commands.py
@@ -44,3 +44,44 @@ def stop(name):
     container = container_api.getContainer(name)
     sysfault.clearFaults(container)
     print('Stopped sysfault on %s' % container.name)
+
+class Premade():
+    '''Class for methods returning premade options'''
+    # Common system calls
+    syscalls = ['open', 'write', 'writev', 'read', 'readv', 'sendfile', 'sendfile64', 'poll', 'select']
+    # Common errors
+    errors = [None, 'EACCES', 'EPERM', 'ENOENT', 'EIO', 'EINTR', 'ENOSYS']
+    # Common delays
+    delays = [None, 1000, 5000]
+
+    # Possible combinations of faults
+    faults = []
+    for s in syscalls:
+        for e in errors:
+            for d in delays:
+                if e is None and d is None: #or (d is not None and e is not None):
+                    continue #Skip, as these are the ones with no effect.
+                fault_string = '-'.join(map(str, filter(
+                                lambda elem : elem is not None, [s,e,d])))
+                faults.append((
+                    fault_string,
+                    sysfault.Fault(syscall=s, error=e, delay_enter=d)))
+
+    def getCombinations(self):
+        '''Possible combinations of faults'''
+        return self.faults
+
+
+autocomplete_faults = Premade().getCombinations()
+def autocompleteFaults(ctx, args, incomplete):
+    return [f for f in autocomplete_faults if incomplete in f[0]]
+
+@fault.command()
+@click.option('--name', prompt='Container name?', autocompletion=cauto.getContainers)
+@click.option('--cmd', prompt='True', autocompletion=autocompleteFaults)
+def premade(name, cmd):
+    '''A bunch of premade auto completed perturbation'''
+    print(len(faults))
+    sysfault.applyFault(container, fault)
+    print('Started sysfault %s on %s' % (fault, container.name))
+
