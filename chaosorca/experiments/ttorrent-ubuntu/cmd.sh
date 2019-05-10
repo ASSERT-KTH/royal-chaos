63fc0370b8c9309198c85aeb4525c87c51c60ac9
Jesper Simonsson <jsimo@kth.se>
2019-05-10 18:05:53 +0200

    Fixes cmd for running ttorrent experiments

--- chaosorca/experiments/ttorrent-ubuntu/cmd.sh
+++ chaosorca/experiments/ttorrent-ubuntu/cmd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chaosorca experiment start --name ttorrent-ubuntu --pid-name "java -jar ttorrent-2.0-client.jar -o output -s 60 ubuntu-18.04.2-desktop-amd64.iso.torrent" --start-cmd "$PWD/run.sh" --stop-cmd "$PWD/stop.sh"
+chaosorca experiment start --name ttorrent-ubuntu --pid-name "java -jar ttorrent-1.5-client.jar -o output -d 5000 -s 60 ubuntu-18.04.2-desktop-amd64.iso.torrent" --start-cmd "$PWD/run.sh" --stop-cmd "$PWD/stop.sh"
