0ddea287b746c10ae7d76022d57a984372998a68
GluckZhang <zhanglong3030@qq.com>
2019-12-19 06:03:58 +0100

    reorganize the dataset, new json file that contains 880 buildable dockerfiles

---
+++ pobs/dataset/pull_dockerfiles.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Filename: pull_dockerfiles.py
+
+import os, sys, shutil, time, json, logging
+from optparse import OptionParser, OptionGroup
+
+__version__ = "0.1"
+
+
+def parse_options():
+    usage = r'usage: python3 %prog [options] -f /path/to/analysis-result.json'
+    parser = OptionParser(usage = usage, version = __version__)
+
+    parser.add_option('-f', '--filepath',
+        action = 'store',
+        type = 'string',
+        dest = 'filepath',
+        help = 'The path to the analysis result (json format)'
+    )
+
+    parser.add_option('-c', '--clean',
+        action = 'store_true',
+        dest = 'clean',
+        help = 'Clean the dataset, remove unrelated projects.'
+    )
+
+    options, args = parser.parse_args()
+    if options.filepath == None:
+        parser.print_help()
+        parser.error("The path to the analysis result should be given.")
+    elif options.filepath != None and not os.path.isfile(options.filepath):
+        parser.print_help()
+        parser.error("%s should be a json file."%options.filepath)
+    
+    return options
+
+def main():
+    options = parse_options()
+
+    if options.clean:
+        shutil.copyfile(options.filepath, '%s.bak'%options.filepath)
+        with open(options.filepath, 'rt') as file:
+            projects = json.load(file)
+            final_dataset = list(filter(lambda project: project["number_of_dockerfiles"] > 0 and project["is_able_to_clone"], projects))
+            for project in final_dataset:
+                project.pop('is_able_to_clone', None)
+                project.pop('is_able_to_build', None)
+                project.pop('is_able_to_run', None)
+                buildable_dockerfiles = list(filter(lambda dockerfile: dockerfile["sanity_check"] == "successful", project["info_from_dockerfiles"]))
+                for dockerfile in buildable_dockerfiles:
+                    dockerfile.pop('sanity_check', None)
+                    dockerfile.pop('pobs_base_generation', None)
+                    dockerfile.pop('pobs_application_build', None)
+                    dockerfile.pop('glowroot_attached', None)
+                    dockerfile.pop('tripleagent_attached', None)
+                    dockerfile.pop('pobs_application_run', None)
+                project["info_from_dockerfiles"] = buildable_dockerfiles
+                project["number_of_dockerfiles"] = len(buildable_dockerfiles)
+            final_dataset = list(filter(lambda project: project["number_of_dockerfiles"] > 0, final_dataset))
+
+        with open(options.filepath, 'wt') as file:
+            json.dump(final_dataset, file, indent = 4)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
