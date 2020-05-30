85cb2a3a317902c8fdfc89eb736fdcbdbb90d945
GluckZhang <zhanglong3030@qq.com>
2020-05-30 22:27:11 +0200

    add a single-column format of results

--- phoebe/experiments/hedwig/result_to_latex.py
+++ phoebe/experiments/hedwig/result_to_latex.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 # Filename: result_to_latex.py
 
-import os, argparse, json
+import os, argparse, json, math
 import logging
 
 TEMPLATE = r"""\begin{table*}[tb]
 \centering
 \caption{Chaos Engineering Experiment Results on HedWig}\label{tab:resultsOfHedwig}
-\begin{tabular}{lrrrrp{5.5cm}}
+\setcounter{rowcount}{-1}
+\begin{tabular}{@{\makebox[3em][r]{\stepcounter{rowcount}\therowcount\hspace*{\tabcolsep}}}lrp{3.2cm}rrp{6.2cm}}
 \toprule
-Target& Error Code& Original Failure Rate& Failure Rate& Injection Count& Result \scriptsize (SF: sending failure, FF: fetching failure, VF: validation failure, SC: server crash, PI: post inspection)\\
+Target& Error Code& Original Failure Rate\newline(min, mean, max)& Fail. Rate& Injection Count& Result \scriptsize (SU: success, SF: sending failure, FF: fetching failure, VF: validation failure, SC: server crash, PI: post inspection)\\
 \midrule
 """ + "%s" + r"""
 \bottomrule
@@ -18,18 +19,46 @@ Target& Error Code& Original Failure Rate& Failure Rate& Injection Count& Result
 \end{table*}
 """
 
+TEMPLATE_SINGLE_COLUMN = r"""\begin{table}[tb]
+\centering
+\scriptsize
+\caption{Chaos Engineering Experiment Results on HedWig}\label{tab:resultsOfHedwig}
+\begin{tabularx}{\columnwidth}{lrRlr}
+\toprule
+Target \& Error& F. Rate& Inj.& Behavioral Assessment Criteria& \\
+\midrule
+""" + "%s" + r"""
+\bottomrule
+\end{tabularx}
+\end{table}
+"""
+
 def handle_args():
     parser = argparse.ArgumentParser(
         description="Summarize experiment results into a latex table.")
     parser.add_argument("-f", "--file", help="the path to the result file (.json)")
+    parser.add_argument("-s", "--single-column", action="store_true", dest="single_column",
+        help="print the table in a single-column format")
     return parser.parse_args()
+
+def round_number(x, sig = 3):
+    return round(x, sig - int(math.floor(math.log10(abs(x)))) - 1)
+
+def human_format(num):
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
 
 def main(args):
     with open(args.file, 'rt') as file:
         data = json.load(file)
         body = ""
         for experiment in data["experiments"]:
-            result = "SF: %d, FF: %d, VF: %d, SC: %d, PI: %s" % (
+            result = "SU:%d, SF:%d, FF:%d, VF:%d, SC:%d, PI:%s" % (
+                experiment["result"]["succeeded"],
                 experiment["result"]["sending_failures"],
                 experiment["result"]["fetching_failures"],
                 experiment["result"]["validation_failures"],
@@ -40,16 +69,26 @@ def main(args):
                 injection_count = experiment["result"]["injection_count"]
             else:
                 injection_count = 1
-            body += "%s& %s& %s& %.3f& %d& %s\\\\\n"%(
-                experiment["syscall_name"],
-                experiment["error_code"][1:], # remove the "-" before the error code
-                "%.3f, %.3f, %.3f"%(experiment["original_min_rate"], experiment["original_mean_rate"], experiment["original_max_rate"]),
-                experiment["failure_rate"],
-                injection_count,
-                result
-            )
+
+            if args.single_column:
+                body += "%s& %s& %s& %s& %s\\\\\n"%(
+                    experiment["syscall_name"],
+                    experiment["error_code"][1:], # remove the "-" before the error code
+                    round_number(experiment["failure_rate"]),
+                    human_format(injection_count),
+                    result
+                )
+            else:
+                body += "%s& %s& %s& %s& %d& %s\\\\\n"%(
+                    experiment["syscall_name"],
+                    experiment["error_code"][1:], # remove the "-" before the error code
+                    "%s, %s, %s"%(round_number(experiment["original_min_rate"]), round_number(experiment["original_mean_rate"]), round_number(experiment["original_max_rate"])),
+                    round_number(experiment["failure_rate"]),
+                    injection_count,
+                    result
+                )
         body = body[:-1] # remove the very last line break
-        latex = TEMPLATE%body
+        latex = TEMPLATE_SINGLE_COLUMN%body if args.single_column else TEMPLATE%body
         latex = latex.replace("_", "\\_")
         print(latex)
 
--- phoebe/experiments/ttorrent/result_to_latex.py
+++ phoebe/experiments/ttorrent/result_to_latex.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 # Filename: result_to_latex.py
 
-import os, argparse, json
+import os, argparse, json, math
 import logging
 
 TEMPLATE = r"""\begin{table*}[tb]
 \centering
 \caption{Chaos Engineering Experiment Results on TTorrent}\label{tab:resultsOfTTorrent}
 \setcounter{rowcount}{-1}
-\begin{tabular}{@{\makebox[3em][r]{\stepcounter{rowcount}\therowcount\hspace*{\tabcolsep}}}lrp{3.5cm}rrrp{3cm}}
+\begin{tabular}{@{\makebox[3em][r]{\stepcounter{rowcount}\therowcount\hspace*{\tabcolsep}}}lrp{3.2cm}rrrp{3cm}}
 \toprule
 Target& Error Code& Original Failure Rate\newline(min, mean, max)& Failure Rate& Injection Count& Exe. Count& Result\\
 \midrule
@@ -19,11 +19,38 @@ Target& Error Code& Original Failure Rate\newline(min, mean, max)& Failure Rate&
 \end{table*}
 """
 
+TEMPLATE_SINGLE_COLUMN = r"""\begin{table}[tb]
+\centering
+\scriptsize
+\caption{Chaos Engineering Experiment Results on TTorrent}\label{tab:resultsOfTTorrent}
+\begin{tabularx}{\columnwidth}{lrRlr}
+\toprule
+Target \& Error& F. Rate& Inj.& Behavioral Assessment Criteria& \\
+\midrule
+""" + "%s" + r"""
+\bottomrule
+\end{tabularx}
+\end{table}
+"""
+
 def handle_args():
     parser = argparse.ArgumentParser(
         description="Summarize experiment results into a latex table.")
     parser.add_argument("-f", "--file", help="the path to the result file (.json)")
+    parser.add_argument("-s", "--single-column", action="store_true", dest="single_column",
+        help="print the table in a single-column format")
     return parser.parse_args()
+
+def round_number(x, sig = 3):
+    return round(x, sig - int(math.floor(math.log10(abs(x)))) - 1)
+
+def human_format(num):
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
 
 def summarize_result(result):
     percentage = dict()
@@ -44,17 +71,32 @@ def main(args):
         data = json.load(file)
         body = ""
         for experiment in data["experiments"]:
-            body += "%s& %s& %s& %.3f& %d& %d& %s\\\\\n"%(
-                experiment["syscall_name"],
-                experiment["error_code"][1:], # remove the "-" before the error code
-                "%.6f, %.6f, %.6f"%(experiment["original_min_rate"], experiment["original_mean_rate"], experiment["original_max_rate"]),
-                experiment["failure_rate"],
-                experiment["result"]["injection_count"],
-                experiment["result"]["rounds"],
-                summarize_result(experiment["result"])
-            )
+            if args.single_column:
+                result = "SU:%d, VF:%d, ST:%d, CR:%d"%(
+                    experiment["result"]["succeeded"],
+                    experiment["result"]["validation_failed"],
+                    experiment["result"]["app_stalled"],
+                    experiment["result"]["app_crashed"]
+                )
+                body += "%s:%s& %s& %s& %d& %s\\\\\n"%(
+                    experiment["syscall_name"],
+                    experiment["error_code"][1:], # remove the "-" before the error code
+                    round_number(experiment["failure_rate"]),
+                    human_format(experiment["result"]["injection_count"]),
+                    result
+                )
+            else:
+                body += "%s& %s& %s& %s& %d& %d& %s\\\\\n"%(
+                    experiment["syscall_name"],
+                    experiment["error_code"][1:], # remove the "-" before the error code
+                    "%s, %s, %s"%(round_number(experiment["original_min_rate"]), round_number(experiment["original_mean_rate"]), round_number(experiment["original_max_rate"])),
+                    round_number(experiment["failure_rate"]),
+                    experiment["result"]["injection_count"],
+                    experiment["result"]["rounds"],
+                    summarize_result(experiment["result"])
+                )
         body = body[:-1] # remove the very last line break
-        latex = TEMPLATE%body
+        latex = TEMPLATE_SINGLE_COLUMN%body if args.single_column else TEMPLATE%body
         latex = latex.replace("_", "\\_")
         print(latex)
         print("Don't forget to add the following code into your latex file:")
