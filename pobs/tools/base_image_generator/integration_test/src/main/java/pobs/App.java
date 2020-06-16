f0fb2ed70fefb65863cbda55420567e5923a47b2
GluckZhang <zhanglong3030@qq.com>
2020-06-16 21:58:59 +0200

    use a local server to provide the target file to be downloaded

--- pobs/tools/base_image_generator/integration_test/src/main/java/pobs/App.java
+++ pobs/tools/base_image_generator/integration_test/src/main/java/pobs/App.java
@@ -9,7 +9,7 @@ import java.net.URL;
 public class App {
     public static void main( String[] args ) {
         try {
-            downloadDSN2019BestPaper();
+            downloadTheFile();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -17,8 +17,11 @@ public class App {
         }
     }
 
-    public static void downloadDSN2019BestPaper() throws InterruptedException, IOException {
-        String fileUrl = "https://www.ieee.org/content/dam/ieee-org/ieee/web/org/pubs/conference-latex-template_10-17-19.zip";
+    public static void downloadTheFile() throws InterruptedException, IOException {
+        // String fileUrl = "https://www.ieee.org/content/dam/ieee-org/ieee/web/org/pubs/conference-latex-template_10-17-19.zip";
+        // it's better to set up a local server that provides the file, just in case of network issues
+        // change "172.17.0.1" to the host's local ip address in docker's subnet
+        String fileUrl = "http://172.17.0.1/conference-latex-template_10-17-19.zip";
         String filePath = "./downloaded/conference-latex-template_10-17-19.zip";
 
         File targetFile = new File(filePath);
