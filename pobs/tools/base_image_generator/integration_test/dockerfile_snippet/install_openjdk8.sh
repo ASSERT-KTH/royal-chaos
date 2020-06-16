fbd8813cb106d71216a70b66418d4687db94cde8
GluckZhang <zhanglong3030@qq.com>
2020-06-16 12:54:55 +0200

    enhance openjdk8 installation script

--- pobs/tools/base_image_generator/integration_test/dockerfile_snippet/install_openjdk8.sh
+++ pobs/tools/base_image_generator/integration_test/dockerfile_snippet/install_openjdk8.sh
@@ -1,5 +1,14 @@
-java -version
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
 
-if ! [ $? -eq 0 ]; then
-  apt-get update && apt-get install -y openjdk-8-jdk ;
+if ! exists java; then
+  if exists apt-get; then
+    apt-get update && apt-get install -y openjdk-8-jdk ;
+  elif exists apk; then
+    apk update && apk add openjdk8
+  elif exists yum; then
+    yum install -y java-1.8.0-openjdk-devel
+  fi
 fi
