1c151b0d37c9a5aa15f7ae6fa8432f5038a8587c
GluckZhang <zhanglong3030@qq.com>
2020-06-28 23:40:33 +0200

    analyze new format of the experiment result

--- pobs/tools/base_image_test/result_analyzer.py
+++ pobs/tools/base_image_test/result_analyzer.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Filename: result_analyzer.py
 
-import os, sys, time, json, logging
+import os, sys, time, json, re, logging
 from prettytable import PrettyTable
 from optparse import OptionParser, OptionGroup
 
@@ -30,6 +30,11 @@ def parse_options():
     
     return options
 
+def clean_image_name(name_str):
+    clean_name = name_str.strip()[5:]
+    clean_name = re.split(" as ", clean_name, flags=re.IGNORECASE)[0].strip()
+    return clean_name
+
 def main():
     options = parse_options()
 
@@ -47,18 +52,20 @@ def main():
 
         analyzed_dockerfile_count = 0
         sanity_check_passed_count = 0
+        ori_application_run_count = 0
         pobs_base_generation_passed_count = 0
         pobs_application_build_passed_count = 0
         glowroot_attached_count = 0
         tripleagent_attached_count = 0
         pobs_application_run_passed_count = 0
+        skip_list = ["busybox", "scratch", "hello-world", "progrium/busybox"]
 
         for project in projects:
             if project["number_of_dockerfiles"] > 0:
                 for dockerfile in project["info_from_dockerfiles"]:
                     for base_image in dockerfile["base_images"]:
-                        all_unique_base_images[base_image[4:].strip()] = 1
-                        if base_image[4:].strip() in top_25_base_images:
+                        all_unique_base_images[clean_image_name(base_image)] = 1
+                        if clean_image_name(base_image) in top_25_base_images:
                             top_25_base_images_covered_projects[project["full_name"]] = 1
 
             if "is_able_to_clone" in project and project["is_able_to_clone"]:
@@ -70,17 +77,21 @@ def main():
                 if project["number_of_dockerfiles"] > 0:
                     analyzed_dockerfile_count = analyzed_dockerfile_count + project["number_of_dockerfiles"]
                     for dockerfile in project["info_from_dockerfiles"]:
+                        full_base_image_name = clean_image_name(dockerfile["base_images"][-1])
+                        if full_base_image_name.rsplit(":", 1)[0] in skip_list: continue
                         if dockerfile["sanity_check"] == "successful":
-                            sanity_check_passed_count = sanity_check_passed_count + 1;
-                            experiment_unique_base_images[dockerfile["base_images"][-1][4:].strip()] = 1
-                            if dockerfile["pobs_base_generation"] == "successful":
-                                pobs_base_generation_passed_count = pobs_base_generation_passed_count + 1
-                                augmented_base_images[dockerfile["base_images"][-1][4:].strip()] = 1
-                                if dockerfile["pobs_application_build"] == "successful":
-                                    pobs_application_build_passed_count = pobs_application_build_passed_count + 1
-                                    if dockerfile["glowroot_attached"]: glowroot_attached_count = glowroot_attached_count + 1
-                                    if dockerfile["tripleagent_attached"]: tripleagent_attached_count = tripleagent_attached_count + 1
-                                    if dockerfile["pobs_application_run"] == "successful": pobs_application_run_passed_count = pobs_application_run_passed_count + 1
+                            sanity_check_passed_count = sanity_check_passed_count + 1
+                            if dockerfile["ori_application_run"] == "successful":
+                                ori_application_run_count = ori_application_run_count + 1
+                                experiment_unique_base_images[full_base_image_name] = 1
+                                if dockerfile["pobs_base_generation"] == "successful":
+                                    pobs_base_generation_passed_count = pobs_base_generation_passed_count + 1
+                                    augmented_base_images[full_base_image_name] = 1
+                                    if dockerfile["pobs_application_build"] == "successful":
+                                        pobs_application_build_passed_count = pobs_application_build_passed_count + 1
+                                        if dockerfile["glowroot_attached"]: glowroot_attached_count = glowroot_attached_count + 1
+                                        if dockerfile["tripleagent_attached"]: tripleagent_attached_count = tripleagent_attached_count + 1
+                                        if dockerfile["pobs_application_run"] == "successful": pobs_application_run_passed_count = pobs_application_run_passed_count + 1
 
         logging.info("the total number of projects covered by the top 25 base images: %d"%len(top_25_base_images_covered_projects))
         logging.info("the total number of unique base images: %d"%len(all_unique_base_images))
@@ -91,11 +102,16 @@ def main():
         logging.info("run_project_count: %d"%run_project_count)
         logging.info("analyzed_dockerfile_count: %d"%analyzed_dockerfile_count)
         logging.info("sanity_check_passed_count: %d"%sanity_check_passed_count)
+        logging.info("ori_application_run_count: %d"%ori_application_run_count)
         logging.info("pobs_base_generation_passed_count: %d"%pobs_base_generation_passed_count)
         logging.info("pobs_application_build_passed_count: %d"%pobs_application_build_passed_count)
         logging.info("glowroot_attached_count: %d"%glowroot_attached_count)
         logging.info("tripleagent_attached_count: %d"%tripleagent_attached_count)
         logging.info("pobs_application_run_passed_count: %d"%pobs_application_run_passed_count)
+
+        for bi in experiment_unique_base_images:
+            if bi not in augmented_base_images:
+                print(bi)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
