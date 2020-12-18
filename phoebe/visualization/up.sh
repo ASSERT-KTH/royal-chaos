7b498b095f69feeeef561fcfd536ccf60cf59e4a
gluckzhang <zhanglong3030@qq.com>
2020-12-18 18:40:39 +0100

    config storage retention for prometheus

--- phoebe/visualization/up.sh
+++ phoebe/visualization/up.sh
@@ -3,7 +3,9 @@
 docker run -d --name prometheus \
   -p 9090:9090 \
   -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
-  prom/prometheus
+  prom/prometheus \
+  --storage.tsdb.retention.time=3y \
+  --config.file=/etc/prometheus/prometheus.yml
 
 sleep 1
 
