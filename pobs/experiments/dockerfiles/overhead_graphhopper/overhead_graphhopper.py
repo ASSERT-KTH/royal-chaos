9f9c0803394a4461f6be3b66df71a4be1fb77e88
gluckzhang <zhanglong3030@qq.com>
2022-05-23 00:54:36 +0200

    component-wise performance evaluation using graphhopper

--- pobs/experiments/dockerfiles/overhead_graphhopper/overhead_graphhopper.py
+++ pobs/experiments/dockerfiles/overhead_graphhopper/overhead_graphhopper.py
@@ -3,7 +3,7 @@
 
 import os, time, re, datetime, tempfile, subprocess, signal
 import logging
-import numpy
+import math, numpy
 from influxdb import InfluxDBClient
 
 PERF = None
@@ -17,7 +17,7 @@ def handle_sigint(sig, frame):
 def workload_generator(duration):
     t_end = time.time() + 60 * duration
     tmp_response = "./response.tmp"
-    correct_response = '{"hints":{"visited_nodes.sum":128,"visited_nodes.average":128.0},"info":{"copyrights":["GraphHopper","OpenStreetMap contributors"],"took":1},"paths":[{"distance":6197.582,"weight":536.020699,"time":535952,"transfers":0,"snapped_waypoints":"m}c`IutfpA~zBwvK"}]}'
+    correct_response = '{"hints":{"visited_nodes.sum":94,"visited_nodes.average":94.0},"info":{"copyrights":["GraphHopper","OpenStreetMap contributors"],"took":1},"paths":[{"distance":6197.582,"weight":536.019699,"time":535952,"transfers":0,"snapped_waypoints":"m}c`IutfpA~zBwvK"}]}'
     cmd_workload = "curl -s -w '%%{time_total}\n' -o %s 'localhost:8989/route?point=52.61822,13.310533&point=52.59833,13.37575&vehicle=car&locale=de&calc_points=false'"%tmp_response
     response_time_list = list()
 
@@ -52,7 +52,7 @@ def get_all_pids(container_name):
 
 def get_cpu_instructions(perf_output_str):
     pattern_cpu_instructions = re.compile(r'([0-9][0-9,]+)\s+instructions')
-    match = pattern_cpu_instructions.search(summary)
+    match = pattern_cpu_instructions.search(perf_output_str)
     cpu_instructions = int(match.group(1).replace(",", ""))
 
     return cpu_instructions
@@ -67,8 +67,9 @@ def cadvisor_metrics(container_name, duration):
     memory_mean = numpy.mean([p["mean"] for p in memory_usage.get_points("memory_usage") if p["mean"] is not None])
     return {"cpu_mean": cpu_mean, "memory_mean": memory_mean}
 
-
 def main():
+    global PERF
+
     container_name = "graphhopper"
     perf_output_file = "./perf.log"
     response_time_list = list()
@@ -77,15 +78,17 @@ def main():
     cpu_instructions_list = list()
     cpu_usage_list = list()
     memory_usage_list = list()
-    
-    for i in range(10):
-        logging.info("round %d started"%(i+1))
-        pids_of_graphhopper = get_all_pids()
+
+    execution_count = 0
+    while execution_count < 10:
+        logging.info("round %d started"%(execution_count+1))
+        pids_of_graphhopper = get_all_pids(container_name)
         perf_cmd = "perf stat -e instructions -o %s -p %s"%(perf_output_file, pids_of_graphhopper)
 
         PERF = subprocess.Popen(perf_cmd, close_fds=True, shell=True, preexec_fn=os.setsid)
         sc, fc, response_time = workload_generator(1)
         if (PERF != None): os.killpg(os.getpgid(PERF.pid), signal.SIGINT)
+        time.sleep(1) # wait for perf to generate its results
 
         sc_list.append(sc)
         fc_list.append(fc)
@@ -99,21 +102,24 @@ def main():
         cpu_and_memory_usage = cadvisor_metrics(container_name, "1m")
         cpu_mean = cpu_and_memory_usage["cpu_mean"]
         memory_mean = cpu_and_memory_usage["memory_mean"]
-        cpu_usage_list.append(cpu_mean)
-        memory_usage_list.append(memory_mean)
-
-        logging.info("round %d finished"%(i+1))
-        logging.info("sc: %d, fc: %d, response_time: %f"%(sc, fc, response_time))
-        logging.info("cpu instructions: %d, cpu usage: %.1f%%, memory usage: %.1f MB"%(cpu_instructions, cpu_mean*100, memory_mean/1000000))
+        if not math.isnan(cpu_mean) and not math.isnan(memory_mean):
+            cpu_usage_list.append(cpu_mean)
+            memory_usage_list.append(memory_mean)
+            execution_count = execution_count + 1
+            logging.info("round %d finished"%(execution_count+1))
+            logging.info("sc: %d, fc: %d, response_time: %f"%(sc, fc, response_time))
+            logging.info("cpu instructions: %d, cpu usage: %.1f%%, memory usage: %.1f MB"%(cpu_instructions, cpu_mean*100, memory_mean/1000000))
+        else:
+            logging.warning("round %d failed to query cpu and memory usage"%(execution_count+1))
         time.sleep(1)
 
     logging.info("done!")
     logging.info("response_time: %f"%numpy.mean(response_time_list))
     logging.info("sc_list: %s"%sc_list)
     logging.info("fc_list: %s"%fc_list)
-    logging.info("cpu instructions: %d"%numpy.mean(cpu_instructions_list)
-    logging.info("cpu usage: %.1f%%"%numpy.mean(cpu_usage_list))
-    logging.info("memory usage: %.1f MB"%numpy.mean(memory_usage_list))
+    logging.info("cpu instructions: %d"%numpy.mean(cpu_instructions_list))
+    logging.info("cpu usage: %.1f%%"%(numpy.mean(cpu_usage_list)*100))
+    logging.info("memory usage: %.1f MB"%(numpy.mean(memory_usage_list)/1000000))
 
 if __name__ == '__main__':
     logger_format = '%(asctime)-15s %(levelname)-8s %(message)s'
